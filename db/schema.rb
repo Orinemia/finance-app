f14a1872accec1ba3de228354eeb4d0fdc166102
Orinemia <o.ajulo.13@aberdeen.ac.uk>
2014-03-19 06:00:38 +0000

    Complete Tests for Users and authentication

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140317222233) do
+ActiveRecord::Schema.define(version: 20140319050146) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20140317222233) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,13 +5,17 @@ require 'spec_helper'
 describe User do
 
 	before do 
-		 @user = User.new(name: "Example User", email: "user@example.com")
+		 @user = User.new(name: "Example User", email: "user@example.com", password: "partytoextreme", password_confirmation: "partytoextreme")
 	end
 
 	subject { @user }
 
 	it { should respond_to(:name) }
 	it { should respond_to(:email) }
+	it { should respond_to(:password_digest) }
+	it { should respond_to(:password) }
+	it { should respond_to(:password_confirmation) }
+	it { should respond_to(:authenticate) }
 
 	it { should be_valid }
 
@@ -49,4 +53,27 @@ describe User do
 			end
 		end
 	end
+
+	describe "when email address is already taken" do
+		before do
+			user_with_same_email = @user.dup
+			user_with_same_email.email = @user.email.upcase
+			user_with_same_email.save
+		end
+		it { should_not be_valid}
+	end
+
+	describe "when password is not present" do
+		before do
+			@user = User.new(name: "Example User", email: "user@example.com", password: " ", password_confirmation: " ")
+		end
+	    it { should_not be_valid}
+	end
+
+	describe "when password doesn't match confirmation" do
+		before { @user.password_confirmation = "mismatch" }
+		it { should_not be_valid }
+	end
+
+	describe ""
 end
