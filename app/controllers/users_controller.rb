22125d39790f97e50ca3699968d8c26036c1d88c
Orinemia <o.ajulo.13@aberdeen.ac.uk>
2014-03-21 18:52:34 +0000

    logout pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,8 @@ class UsersController < ApplicationController
 
   def create
   	@user = User.new(user_params)     
-  	if @user.save 
+  	if @user.save
+        sign_in @user 
   		  flash[:success] = "Welcome to the Finance App!"
   	      redirect_to @user
     else
